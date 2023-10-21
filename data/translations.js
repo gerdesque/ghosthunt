85342511efa1e87bfb2ef92c99447600fcf9f75b
gerdesque <gerdesque@users.noreply.github.com>
2023-10-21 12:55:01 +0000

    Deploying to gh-pages from @ gerdesque/ghosthunt@7023c1389e06958cd30f36bb3bd6025671c30b13 🚀

---
+++ data/translations.js
@@ -0,0 +1,142 @@
+getTranslations = function () {
+  text = {};
+  text["StoryCaption"] = {
+    "1": {
+      de: 'Menu',
+      en: 'Menu'
+    },
+    "2": {
+      de: 'Punkte:',
+      en: 'Points:'
+    },
+    "3": {
+      de: 'Gefundene Runen:',
+      en: 'Runes found:'
+    }
+  };
+  text["StoryMenu"] = {
+    "1": {
+      de: 'Geister',
+      en: 'Ghosts'
+    },
+    "2": {
+      de: 'Hilfe',
+      en: 'Help'
+    },
+    "3": {
+      de: 'Impressum',
+      en: 'Legal Notice'
+    }
+  };
+  text["Dev"] = {
+    "1": {
+      de: 'Karte',
+      en: 'Map'
+    }
+  }
+  text["setup.0"] = {
+    "1": {
+      de: 'Diese Rallye befindet sich in der Entwicklung und ist nicht das finale Produkt. Diese Version ist nur für Testzwecke bestimmt.',
+      en: 'This rally is a test version and not the final product. It is intended for testing purposes only.'
+    }
+  }
+  text["story.init"] = {
+    "1": {
+      de: 'Achtung!',
+      en: 'Attention!'
+    },
+    "2": {
+      de: 'Paranormale Aktivität entdeckt.',
+      en: 'Paranormal activity detected.'
+    }
+  }
+  text["map"] = {
+    "1": {
+      de: 'Diese Karte ist dein Hauptbildschirm.',
+      en: 'This map is your main screen.'
+    },
+    "2": {
+      de: 'Hier siehst du alle Hotspots in deiner Umgebung.',
+      en: 'Here you can see all the hotspots in your area.'
+    },
+    "3": {
+      de: 'Laufe in einen der Hotspots hinein! Sobald er gelb wird, kannst du ihn anklicken.',
+      en: 'Run into one of the hotspots! As soon as it turns yellow, you can click on it.'
+    }
+  }
+  text["story.1"] = {
+    "1": {
+      de: 'Du hast ein paranormales Feld betreten. Zeit die Geister aufzuspüren. Dein Scanner sucht gerade nach Geister-Echos. Echos sind verblasste Erinnerungen oder Emotionen. Sie binden die Geister an einen spezifischen Ort. Schau dir die Fotos an und versuche die Rätsel zu lösen. Die Antworten befinden sich ganz in der Nähe.',
+      en: 'You have entered a paranormal field. Time to track down the ghosts. Your scanner is currently searching for ghost echoes. Echoes are faded memories or emotions. They tie the ghosts to a specific location. Look at the photos and try to solve the puzzles. The answers are close by.'
+    },
+  }
+  text["vorstellung"] = {
+    "1": {
+      de: 'Hallo Geisterjäger. Und willkommen zur Testversion von Spuk & Schreck. Wir sind Anne, Gerd und Gregor. Die Entwickler dieser interaktiven Geister-Rallye. Diese Rallye ist für Familien, Freunde und Leute gemacht, die. sich gern gruseln und gemeinsam Zeit an der frischen Luft verbringen. Wir überprüfen jetzt, ob dein Handy die nötigen Voraussetzungen erfüllt. Und dann geht es auch schon los.',
+      en: 'Hello ghost hunters. And welcome to the test version of Spuk & Schreck. We are Anne, Gerd and Gregor. The developers of this interactive ghost rally. This rally is made for families, friends and people who. like to be spooked and spend time together in the fresh air. We\'re going to check if your phone meets the necessary requirements. And then we\'ll get started.'
+    },
+  }
+  text["Impressum"] = {
+    "1": {
+      de: 'Impressum',
+      en: 'Legal Notice'
+    }
+  }
+  text["Inventar"] = {
+    "1": {
+      de: 'Geister',
+      en: 'Ghosts'
+    },
+    "2": {
+      de: 'Hier werden alle Geister angezeigt, die du mit der AR-Kamera eingefangen hast.',
+      en: 'All the ghosts you captured with the AR camera are displayed here.'
+    },
+    "3": {
+      de: 'AR-Kamera öffnen',
+      en: 'Open AR camera'
+    }
+  }
+  text["Hilfe"] = {
+    "1": {
+      de: 'Hilfe',
+      en: 'Help'
+    },
+    "2": {
+      de: 'Erkunde die Stationen auf der Karte, um geheime Geister zu finden und Buchstaben für das Lösungswort zu sammeln.',
+      en: 'Explore the stations on the map to find secret ghosts and collect and collect letters for the solution word.'
+    },
+    "3": {
+      de: 'Wo finde ich meinen Spielfortschritt?',
+      en: 'Where can I find my game progress?'
+    },
+    "4": {
+      de: 'Deinen Spielfortschritt kannst du hier einsehen:',
+      en: 'You can check your game progress here:'
+    },
+    "5": {
+      de: 'Was passiert, wenn ich das Lösungswort vollständig erraten habe?',
+      en: 'What happens when I have completely guessed the solution word?'
+    },
+    "6": {
+      de: 'Sed non urna. Donec et ante. Phasellus eu ligula. Vestibulum sit amet purus.',
+      en: 'Sed non urna. Donec et ante. Phasellus eu ligula. Vestibulum sit amet purus.'
+    },
+    "7": {
+      de: 'Benötigt jeder Mitspielende ein eigenes Handy?',
+      en: 'Does each player need their own cell phone?'
+    },
+    "8": {
+      de: 'Wir empfehlen das Spiel als Gruppe mit einem Handy zu spielen. Natürlich könnte ihr aber auch jeder alleine mit einem Handy losziehen. Es gibt keine vorgeschriebene Reihenfolge für die Stationen. Am Ende könnt ihr vergleichen, wer mehr Punkte und Geister gesammelt hat.',
+      en: 'We recommend playing the game as a group with a cell phone. Of course, you could also each go off on your own with a cell phone. There is no prescribed order for the stations. At the end you can compare who has collected more points and ghosts.'
+    },
+    "9": {
+      de: 'Wie kann ich die Rätsel lösen?',
+      en: 'How can I solve the puzzles?'
+    },
+    "10": {
+      de: 'Die Rätsel im Spiel können nur durch genaues Absuchen eurer Umgebung gelöst werden. Findet die Orte auf dem Bild und studiert sie genau. Dann findet ihr die Antwort.',
+      en: 'The puzzles in the game can only be solved by carefully scanning your surroundings. Find the places on the picture and study them carefully. Then you will find the answer.'
+    },
+  }
+  return text;
+};
--- service-worker.js
+++ service-worker.js
@@ -1,99 +1,36 @@
 // Files to cache
-const cacheName = "ghosthunt-v0.1";
-const urlsToCacheiOS = [
-  'index.html',
-  'images/bg.jpg',
-  'images/menu.jpg',
-  'fonts/Creepster/Creepster-Regular.ttf',
-  'fonts/para.otf',
-  'images/icon.png',
-  'videos/intro.1.mp4',
-  'models/setup.3/setuup.3.usdz',
-  'audio/dist/audio/Voices, Laugh, Male, Mysterious Laughter SND67498.wav',
-  'images/holger.png',
-  'images/holger.bg.jpg',
-  'videos/intro.2.mp4',
-  'videos/intro.3.mp4',
-  'videos/intro.4.mp4',
-  'videos/intro.6.mp4',
-  'images/paranormal.jpg',
-  'images/cam.jpg',
-  'videos/1.1.mp4',
-  'models/kapitel.1/kapitel.1.usdz',
-  'videos/1.3.mp4',
-  'videos/1.5.mp4',
-  'models/herbert/herbert.usdz',
-  'models/kapitel.2/kapitel.2.usdz',
-  'models/rosa/rosa.usdz'
+const cacheName = 'ghosthunt-v1';
+const contentToCache = [
+  '/index.html'
 ];
 
-const urlsToCacheAndroid = [ 
-  'index.html',
-  'images/bg.jpg',
-  'images/menu.jpg',
-  'fonts/Creepster/Creepster-Regular.ttf',
-  'fonts/para.otf',
-  'images/icon.png',
-  'videos/intro.1.mp4',
-  'models/setup.3/setuup.3.glb',
-  'audio/dist/audio/Voices, Laugh, Male, Mysterious Laughter SND67498.wav',
-  'images/holger.png',
-  'images/holger.bg.jpg',
-  'videos/intro.2.mp4',
-  'videos/intro.3.mp4',
-  'videos/intro.4.mp4',
-  'videos/intro.6.mp4',
-  'images/paranormal.jpg',
-  'images/cam.jpg',
-  'videos/1.1.mp4',
-  'models/kapitel.1/kapitel.1.glb',
-  'videos/1.3.mp4',
-  'videos/1.5.mp4',
-  'models/herbert/herbert.glb',
-  'models/kapitel.2/kapitel.2.glb',
-  'models/rosa/rosa.glb'
-  ];
+// Installing Service Worker
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Install');
+  e.waitUntil((async () => {
+    const cache = await caches.open(cacheName);
+    console.log('[Service Worker] Caching all: app shell and content');
+    await cache.addAll(contentToCache);
+  })());
+});
 
-  const self = this;
+// Fetching content using Service Worker
+self.addEventListener('fetch', (e) => {
+    // Cache http and https only, skip unsupported chrome-extension:// and file://...
+    if (!(
+       e.request.url.startsWith('http:') || e.request.url.startsWith('https:')
+    )) {
+        return; 
+    }
 
-  // Install SW
-  self.addEventListener('install', (event) => {
-      event.waitUntil(
-          caches.open(CACHE_NAME)
-              .then((cache) => {
-                  console.log('Opened cache');
-  
-                  const is_safari = navigator.userAgent.indexOf('Safari') > -1 && navigator.userAgent.indexOf('Chrome') <= -1;
-  
-                  return cache.addAll(is_safari ? urlsToCacheiOS : urlsToCacheAndroid);
-              })
-      )
-  });
-  
-  // Listen for requests
-  self.addEventListener('fetch', (event) => {
-      event.respondWith(
-          caches.match(event.request)
-              .then(() => {
-                  return fetch(event.request)
-                      .catch(() => caches.match('offline.html'))
-              })
-      )
-  });
-  
-  // Activate the SW
-  self.addEventListener('activate', (event) => {
-      const cacheWhiteList =[];
-      cacheWhiteList.push(CACHE_NAME);
-  
-      event.waitUntil(
-          caches.keys().then((cacheName) => Promise.all(
-               cacheName.map((cacheName) => {
-                   if(!cacheWhiteList.includes(cacheName)) {
-                       return caches.delete(cacheName);
-                   }
-               })
-          ))
-      )
-  });
-  
+  e.respondWith((async () => {
+    const r = await caches.match(e.request);
+    console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+    if (r) return r;
+    const response = await fetch(e.request);
+    const cache = await caches.open(cacheName);
+    console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+    cache.put(e.request, response.clone());
+    return response;
+  })());
+});
